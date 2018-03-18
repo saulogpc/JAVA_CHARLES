f190acda57f35eb0865d2b1487a62c825b9fa833
saulogpc <saulogpc@yahoo.com.br>
2018-03-17 21:36:16 -0300

    Olá!

---
+++ src/Ola.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Ola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Olá!");
+
+	}
+
+}
